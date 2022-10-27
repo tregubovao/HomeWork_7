2b071839671eca7ddec5dfe28e7ad9c62d24dc63
Aleksandr <tregubovao@mail.ru>
2022-10-27 16:18:08 +0300

    Задача_7_

---
+++ Задача_7/Program.cs
@@ -0,0 +1,50 @@
+﻿// // Написать программу, которая обменивает элементы первой строки и последней строки
+void FiilArray(int[,] matrix, int m, int n)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(0, 10);            
+        }        
+    }
+}
+
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix [i,j]} ");
+        Console.WriteLine();
+    }
+}
+
+void ChangesStrings(int[,] matrix)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        int buferobmena = matrix[0, j];
+        matrix[0, j] = matrix[matrix.GetLength(0) - 1, j];
+        matrix[matrix.GetLength(0) - 1, j] = buferobmena;
+    }
+}
+
+Console.WriteLine("Введите количество строк: ");
+int m = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите количество столбцов: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+
+int[,] matrix = new int[m, n];
+
+Console.Write("Базовый массив: ");
+Console.WriteLine();
+FiilArray(matrix, m, n);
+PrintArray(matrix);
+
+ChangesStrings(matrix);
+
+Console.Write("Измененный массив (первая и последняя строки поменяны местами): ");
+Console.WriteLine();
+PrintArray(matrix);
+
