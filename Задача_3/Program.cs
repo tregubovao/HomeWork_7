f4c41a0321d032e1b2720c9faf1f05623912f913
Aleksandr <tregubovao@mail.ru>
2022-10-27 16:36:46 +0300

    Задача_3_добавлен метод_сокращен код

--- Задача_3/Program.cs
+++ Задача_3/Program.cs
@@ -1,5 +1,38 @@
 ﻿// В двумерном массиве заменить элементы, у которых оба индекса чётные на их квадраты
 
+void FiilArray(int[,] matrix, int m, int n)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(0, 10);            
+        }        
+    }
+}
+
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix [i,j]} ");
+        Console.WriteLine();
+    }
+}
+
+void ZamenaNaKvadrat(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0)
+                matrix[i, j] = matrix[i, j] * matrix[i, j];            
+        }       
+    }
+}
+
 Console.WriteLine("Введите количество строк: ");
 int m = int.Parse(Console.ReadLine() ?? "0");
 Console.WriteLine("Введите количество столбцов: ");
@@ -9,25 +42,11 @@ int[,] matrix = new int[m, n];
 
 Console.Write("Базовый массив: ");
 Console.WriteLine();
-for (int i = 0; i < m; i++)
-{
-    for (int j = 0; j < n; j++)
-    {
-        matrix[i, j] = new Random().Next(0, 10);
-        Console.Write($"{matrix[i, j]} ");
-    }
-    Console.WriteLine();
-}
+FiilArray(matrix, m, n);
+PrintArray(matrix);
 
-Console.Write("Измененный массив: ");
+ZamenaNaKvadrat(matrix);
+
+Console.Write("Измененный массив (заменены элементы, у которых оба индекса чётные на их квадраты): ");
 Console.WriteLine();
-for (int i = 0; i < m; i++)
-{
-    for (int j = 0; j < n; j++)
-    {
-        if (i % 2 == 0 && j % 2 == 0)
-            matrix[i, j] = matrix[i, j] * matrix[i, j];
-        Console.Write($"{matrix[i, j]} ");
-    }
-    Console.WriteLine();
-}
+PrintArray(matrix);
