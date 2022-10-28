ec12d9cb8072edb68e89a1dd25d9dce717d96d32
Aleksandr <tregubovao@mail.ru>
2022-10-28 13:39:49 +0300

    Задача_1_добавлены методы

--- Задача_1/Program.cs
+++ Задача_1/Program.cs
@@ -1,5 +1,27 @@
 ﻿// Показать двумерный массив размером m×n заполненный вещественными числами
 
+void FiilArray(double[,] matrix, int m, int n)
+{
+    Random random = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = random.NextDouble() * 100;            
+        }        
+    }
+}
+
+void PrintArray(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix [i,j]} ");
+        Console.WriteLine();
+    }
+}
+
 Console.WriteLine("Введите количество строк: ");
 int m = int.Parse(Console.ReadLine() ?? "0");
 Console.WriteLine("Введите количество столбцов: ");
@@ -7,14 +29,7 @@ int n = int.Parse(Console.ReadLine() ?? "0");
 
 double[,] matrix = new double[m, n];
 
-Random random = new Random();
-for (int i = 0; i < m; i++)
-{
-    for (int j = 0; j < n; j++)
-    {
-        matrix[i, j] = random.NextDouble() * 100;
-        Console.Write($"{matrix[i, j]} ");
-    }
-    Console.WriteLine();
-}
-
+Console.Write("Ваш массив: ");
+Console.WriteLine();
+FiilArray(matrix, m, n);
+PrintArray(matrix);
